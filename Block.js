889f88acf1e9538679224df428bc4acbcdc8b096
Lior Amsalem <lior.a@fiverr.com>
2017-12-02 19:59:57 +0200

    Add constructor, calculate block hash, and mine method to Block module

--- Block.js
+++ Block.js
@@ -7,6 +7,33 @@ const crypto = require('crypto-js');
 
 class Block {
 
+
+
+    constructor(id, timestamp, data, prevHash = '') {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = this.calculateBlockHash();
+        this.n = 0;
+    }
+
+    calculateBlockHash() {
+        return crypto.SHA256(this.n + this.id + this.prevHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+    mineNewBlock(difficulty) {
+        if (difficulty < 1) {
+            throw new Error('Difficulty can\'t be lower than 1');
+        }
+
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.n++;
+            this.hash = this.calculateBlockHash();
+        }
+
+        console.log('Block Mined: ' + this.hash);
+    }
 }
 
 module.exports = Block;
